d163f8826539f393000fed37a0e0f33069c58315
yeh <md32513@gmail.com>
2021-02-07 00:41:10 +0800

    refactor single condition & range condition

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -10,28 +10,27 @@ fun main(args: Array<String>) {
     // 光環出現條件：走運且健康值>50
     // 邏輯運算子優先順序：[!], [<,<=,>,>=], [==,!=], [&&], [||]
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    if(auraVisible){
-        println("GREEN")
-    }else{
-        println("NONE")
-    }
+    // 光環顏色判斷，單行條件更簡潔重構
+    val auraColor = if(auraVisible) "GREEN" else "NONE"
+    println(auraColor)
 
-    //  判斷玩家狀況
+
+    //  判斷玩家狀況，改用range重構指定範圍，可解決條件句需要排序的問題
     val healthStatus = if(healthPoints == 100){
-        " is in excellent condition!"
-    }else if(healthPoints >= 90){
-        " has a few scratches."
-    }else if(healthPoints >= 75){
+        "is in excellent condition!"
+    }else if(healthPoints in 90..99){
+        "has a few scratches."
+    }else if(healthPoints in 75..89){
         // 巢狀if...else
         if(isBlessed){
-            " has some minor wounds but is healing quite quickly!"
+            "has some minor wounds but is healing quite quickly!"
         }else{
-            " has some minor wounds."
+            "has some minor wounds."
         }
-    }else if(healthPoints >= 15){
-        " looks pretty hurt."
+    }else if(healthPoints in 15..74){
+        "looks pretty hurt."
     }else{
-        " is in awful condition!"
+        "is in awful condition!"
     }
     println(name + " " + healthStatus)
 }
