6cdef8e2e15e64c2e4022899373f60c46d3cbb21
yeh <md32513@gmail.com>
2021-02-07 02:31:06 +0800

    refactor function & build new function

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -8,30 +8,50 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
     // 光環出現條件：走運且健康值>50
-    // 邏輯運算子優先順序：[!], [<,<=,>,>=], [==,!=], [&&], [||]
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    // 光環顏色判斷，單行條件更簡潔重構
-    val auraColor = if(auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
     // 判斷玩家狀況
-    // 用when改寫，比對when小括號中的引數==大括弧中->運算子左邊條件
-    // 條件句若有else if分支，都建議改用when寫
-    val healthStatus = when(healthPoints){
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // 顯示玩家狀況
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    // 施展Fireball魔法
+    castFireball()
+}
+
+// 使用Refactor→function..重構
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+            )
+    println("$name $healthStatus")
+}
+
+// 使用Refactor→function..重構
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+// 使用Refactor→function..重構
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
-        in 75..89 -> if(isBlessed){
-            "has some minor wounds but is healing quite quickly!"
-        }else{
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
             "has some minor wounds."
         }
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
+    return healthStatus
+}
 
-    // $符號可在字串中用${}計算運算式的值，其結果會被插入字串
-    println("(Aura: $auraColor) " +
-            "(Blessed: ${if(isBlessed) "YES" else "NO"})")
-
-    // $符號可使變數插入字串中
-    println("$name $healthStatus")
+// 自訂函數Fireball魔法，輸出施展魔法的結果
+private fun castFireball(){
+    println("A glass of Fireball springs into existence.")
 }
