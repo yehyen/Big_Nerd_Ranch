fa3a18122ac1504860cf436df1395943a43b22d2
yeh <md32513@gmail.com>
2021-02-09 03:00:59 +0800

    Anonymous Function as a variable

--- Sandbox/src/SimVillage.kt
+++ Sandbox/src/SimVillage.kt
@@ -1,8 +1,13 @@
 fun main(args: Array<String>) {
 
-    // 匿名函數，輸入歡迎語向新鎮長致敬
-    println({
+    // 輸入歡迎語向新鎮長致敬，匿名函數當作變數值指派給函數類型的變數
+    // 一般變數定義：val greetingFunction: String = ... 表示greetingFunction儲存的是哪種類型的資料
+    // 一般函數定義：fun greetingFunction(): String{}
+    // 匿名函數類型的變數定義：val greetingFunction: () -> String = ...表示greetingFunction儲存的是哪種類型的函數
+    // 如上() -> String，()的參數，返回資料類型為String
+    val greetingFunction: () -> String = {
         val currentYear = 2018
         "Welcom to SimVillage, Mayor! (copyright $currentYear)"
-    }())
+    }
+    println(greetingFunction())
 }
