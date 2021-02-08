28a76e298714a76091558ac1a46cd3bdcf088579
yeh <md32513@gmail.com>
2021-02-09 04:03:09 +0800

    argument type in anonymous function & regular functions

--- Sandbox/src/SimVillage.kt
+++ Sandbox/src/SimVillage.kt
@@ -8,10 +8,18 @@ fun main(args: Array<String>) {
     // 不用註明return，匿名函數會自動回傳函數中最後一行
     // ()內放匿名函數的參數類型，playerName為()的參數名稱
     // 只有一個參數的匿名函數，可使用it代替參數名稱或具名引數
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+    // 類型推斷特性可簡略定義類型，讓greetingFunction函數自行去推斷，但參數必須要有名稱和類型定義，否則程式混洧
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcom to SimVillage, $playerName! (copyright $currentYear)"
     }
-    println(greetingFunction("Guyal", 2))
+
+    // 隨機確定已建造多少棟房子後，輸出歡迎語句
+    runSimulation("Guyal", greetingFunction)
+}
+
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
