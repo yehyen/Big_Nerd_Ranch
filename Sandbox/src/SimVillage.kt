42799edb50d50b683c7c8c11d332f715b3f3c3ff
yeh <md32513@gmail.com>
2021-02-09 02:38:26 +0800

    initial commit for Sandbox

---
+++ Sandbox/src/SimVillage.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    // 匿名函數，輸入歡迎語向新鎮長致敬
+    println({
+        val currentYear = 2018
+        "Welcom to SimVillage, Mayor! (copyright $currentYear)"
+    }())
+}
